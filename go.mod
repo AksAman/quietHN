8eb954da36ee9611707e5e92a094289872dd316a
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-09-21 04:59:15 +0530

    wip hnclient

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/AksAman/gophercises/quietHN
-
-go 1.19
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/AksAman/gophercises/quiet-hn/hnclient"
-
-func main() {
-	client := hnclient.Client{}
-}
