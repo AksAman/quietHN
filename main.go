1ea4da846147902373f30a81d8fb2e7282b6714b
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-09-29 10:04:14 +0530

    add complete routing in routing.go from main.go

--- main.go
+++ main.go
@@ -7,21 +7,12 @@ import (
 
 	"github.com/AksAman/gophercises/quietHN/routing"
 	"github.com/AksAman/gophercises/quietHN/settings"
-	"github.com/AksAman/gophercises/quietHN/views"
-	"github.com/gorilla/mux"
 )
 
 func RunServer() {
-	router := mux.NewRouter()
-	router.NotFoundHandler = views.NotFoundHandler{}
-
-	for _, route := range routing.Routes {
-		router.HandleFunc(route.Pattern, route.Handler)
-	}
-
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%d", settings.Settings.Port),
-		Handler: router,
+		Handler: routing.NewRouter(),
 	}
 
 	fmt.Printf("Starting server on %s\n", server.Addr)
--- routing/routing.go
+++ routing/routing.go
@@ -4,14 +4,27 @@ import (
 	"net/http"
 
 	"github.com/AksAman/gophercises/quietHN/views"
+	"github.com/gorilla/mux"
 )
 
 type URLRoute struct {
 	Pattern string
 	Handler func(http.ResponseWriter, *http.Request)
+	Methods []string
 }
 
 var Routes = []URLRoute{
-	{Pattern: "/", Handler: views.Home},
-	{Pattern: "/stories/", Handler: views.Stories},
+	{Pattern: "/", Handler: views.Home, Methods: []string{"GET"}},
+	{Pattern: "/stories/", Handler: views.Stories, Methods: []string{"GET"}},
+}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.NotFoundHandler = views.NotFoundHandler{}
+
+	for _, route := range Routes {
+		router.HandleFunc(route.Pattern, route.Handler).Methods(route.Methods...)
+	}
+
+	return router
 }
